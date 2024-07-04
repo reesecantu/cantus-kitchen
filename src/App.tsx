def559c27f35e52f0cd3b7b17669c47b19f334d7
reesecantu <65140498+reesecantu@users.noreply.github.com>
2024-07-04 15:24:25 -0700

    test the functionality in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
-// import { useState, useEffect } from "react";
-import './App.css'
-// import { supabase } from './supabase'
+import "./App.css";
+import {
+  deleteRecipe,
+  getRecipe,
+  insertRecipe,
+} from "./helpers/recipe-helpers";
+import { RecipeType } from "./supabase-types";
 
 function App() {
+  const newRecipe: Omit<RecipeType, "id" | "created_at"> = {
+    category_id: null,
+    price: 9.99,
+    name: "New Recipe",
+    instructions: "Cook the new recipe",
+    link: "https://www.example.com",
+    servings: 4,
+  };
+
+  const handleGetRecipe = async () => {
+    const recipe = await getRecipe(1);
+    console.log("Recipe:", recipe);
+  };
+
+  // TODO sample data, replace with form data
+  const handleInsertRecipe = async () => {
+    const newRecipe: Omit<RecipeType, "id" | "created_at"> = {
+      category_id: null,
+      price: 9.99,
+      name: "New Recipe",
+      instructions: "Cook the new recipe",
+      link: "https://www.example.com",
+      servings: 4,
+    };
+
+    const insertedRecipe = await insertRecipe(newRecipe);
+    console.log("Inserted Recipe:", insertedRecipe);
+  };
+
+  const handleDeleteRecipe = async () => {
+    const success = await deleteRecipe(3);
+    console.log("Recipe deleted:", success);
+  };
 
   return (
     <>
-    <h1>Hi Lily, I love yous</h1>
-    
+      <h1>Hi Lily, I love yous</h1>
+      <button onClick={handleGetRecipe}>Get Recipe</button>
+      <button onClick={handleInsertRecipe}>Insert Recipe</button>
+      <button onClick={handleDeleteRecipe}>Delete Recipe</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Ingredient/IngredientCategory.tsx
+++ src/components/Ingredient/IngredientCategory.tsx
@@ -1,64 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { supabase } from "/supabase";
+import { useCallback, useState, useEffect } from "react";
+import supabase from "../../supabase-client";
+import { IngredientCategoryType } from "../../supabase-types";
 
-function IngredientCategory() {
-  const [name, setName] = useState("");
+function IngredientCategory(ic: IngredientCategoryType) {
+  
+    
 
-  useEffect(() => {
-    getIngredientCategory();
-  }, []);
-
-  async function addIngredientCategory(name: string) {
-    try {
-      const { data, error } = await supabase // Destructuring our Supabase call
-        .from("ingredient_category") // Get our "Groceries" table
-        .insert({ name: name }) // Insert passed in name and price
-        .single(); // Only insert it once
-      if (error) throw error; // If there is an error, throw it
-      window.location.reload(); // Load the window once complete
-    } catch (error) {
-      alert(error); // If an error is caught, alert it on screen
-    }
-  }
-
-  async function getIngredientCategory() {
-    try {
-      const { data, error } = await supabase // Destructuring our Supabase call
-        .from("ingredient_category") // Get our "Groceries" table
-        .select("*"); // Select all data
-      if (error) throw error; // If there is an error, throw it
-      console.log(data); // Log the data to the console
-    } catch (error) {
-      alert(error); // If an error is caught, alert it on screen
-    }
-  }
-
-  async function deleteIngredientCategory({ name }) {
-    try {
-      const { data, error } = await supabase // Destructuring our Supabase call
-        .from("ingredient_category") // Get our "Groceries" table
-        .delete() // Delete the data
-        .eq("name", name); // Equal to the passed in name
-      if (error) throw error; // If there is an error, throw it
-      window.location.reload(); // Load the window once complete
-    } catch (error) {
-      alert(error); // If an error is caught, alert it on screen
-    }
-  }
 
   return (
-    <div>
-      <h1>Ingredient Category</h1>
-      <button onClick={() => addIngredientCategory("Fruit")}>
-        Add Ingredient Category
-      </button>
-      <button onClick={() => deleteIngredientCategory({ name: "Fruit" })}>
-        Delete Ingredient Category
-      </button>
-
-      
-    </div>
+    <>
+      <div>IngredientCategory</div>
+      <p>{ic.name}</p>
+    </>
   );
 }
 
 export default IngredientCategory;
+
+// const [ingredientCategories, setIngredientCategories] =
+// useState<IngredientCategoryType[]>();
+
+// const fetcher = useCallback(async () => {
+// const { data, error } = await supabase
+//   .from("ingredient_category")
+//   .select("*");
+// if (error) {
+//   console.error("error", error);
+// } else {
+//   setIngredientCategories(data);
+// }
+// }, []);
