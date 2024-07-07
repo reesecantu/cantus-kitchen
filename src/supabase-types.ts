d2e1c6eafc809a334f8a6099c7e99ae8c646bc44
reesecantu <65140498+reesecantu@users.noreply.github.com>
2024-07-06 17:50:17 -0700

    Define all supabase types

--- src/supabase-types.ts
+++ src/supabase-types.ts
@@ -1,4 +1,8 @@
 import { Database } from "./schema";
 
+export type IngredientType = Database['public']['Tables']['ingredient']['Row'];
 export type IngredientCategoryType = Database['public']['Tables']['ingredient_category']['Row'];
+export type IngredientUnitType = Database['public']['Tables']['ingredient_unit']['Row'];
 export type RecipeType = Database['public']['Tables']['recipe']['Row'];
+export type RecipeCategoryType = Database['public']['Tables']['recipe_category']['Row'];
+export type RecipeIngredientType = Database['public']['Tables']['recipe_ingredient']['Row'];
