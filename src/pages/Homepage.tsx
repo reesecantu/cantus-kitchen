30039f488d125412b00d493e45cc4173af876120
reesecantu <65140498+reesecantu@users.noreply.github.com>
2024-07-06 22:14:55 -0700

    Add a button on Homepage that redirects to Login

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 function Homepage() {
-  return <h1>Hi Lily, I love yous!</h1>;
+    const navigate = useNavigate();
+  return (
+    <>
+      <h1>Hi Lily, I love yous!</h1>;
+      <button onClick={() => navigate("/login")}>Login here</button>
+    </>
+  )
 }
 
 export default Homepage;
