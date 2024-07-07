6a84368ccc429e152068a3805d3ba097f36d4b00
reesecantu <65140498+reesecantu@users.noreply.github.com>
2024-07-06 21:12:50 -0700

    update App.tsx import

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   if (token) sessionStorage.setItem("token", JSON.stringify(token));
 
   useEffect(() => {
-    if(sessionStorage.getItem("token")) {
+    if (sessionStorage.getItem("token")) {
       let data = JSON.parse(sessionStorage.getItem("token")!);
       setToken(data);
     }
