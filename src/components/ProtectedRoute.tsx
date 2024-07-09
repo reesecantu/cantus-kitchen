5cd19883c661c8e124ee7a000fd2c551c8d81141
reesecantu <65140498+reesecantu@users.noreply.github.com>
2024-07-08 23:30:26 -0700

    feat: Add ProtectedRoute component for authenticated routes

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useAuth } from "../Auth";
+import { Navigate, useLocation } from "react-router-dom";
+
+function ProtectedRoute(children: React.ReactNode) {
+  const { user } = useAuth();
+  const location = useLocation();
+  
+  return user ? (
+    children
+  ) : (
+    <Navigate to="/login" replace state={{ path: location.pathname }} />
+  );
+}
+
+export default ProtectedRoute;
