9a8246a5ef4fb0c2c0170938de497d331bc5b665
reesecantu <65140498+reesecantu@users.noreply.github.com>
2025-07-22 18:22:10 -0700

    Add routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import { Home } from "./pages/Home";
 import { Navbar } from "./components/Navbar";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
 
 function App() {
+  const location = useLocation();
+  const hideNavbar = ["/sign-in", "/sign-up", "/forgot-password"].includes(
+    location.pathname
+  );
+
   return (
     <div>
-      <Navbar />
-      <div>
+      {!hideNavbar && <Navbar />}
+      <div className={hideNavbar ? "" : "pt-20"}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </div>
     </div>
