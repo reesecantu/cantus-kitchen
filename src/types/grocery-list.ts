ada877eb55c5d1b881f90eaa2a136c770f02f102
reesecantu <65140498+reesecantu@users.noreply.github.com>
2025-08-01 21:54:01 -0400

    Add in grocery list related types

---
+++ src/types/grocery-list.ts
@@ -0,0 +1,24 @@
+import type { Tables } from './database-types';
+
+export type GroceryList = Tables<'grocery_lists'>;
+export type GroceryListRecipe = Tables<'grocery_list_recipes'>;
+export type GroceryListItem = Tables<'grocery_list_items'>;
+
+export interface GroceryListWithStats extends GroceryList {
+  recipe_count: number;
+  item_count: number;
+  completed_item_count: number;
+}
+
+export interface GroceryListItemWithDetails extends GroceryListItem {
+  ingredient_name: string;
+  unit_name: string;
+  unit_abbreviation: string;
+  grocery_aisle_id: number;
+  grocery_aisle_name: string;
+}
+
+export interface GroceryListFull extends GroceryList {
+  recipes: (GroceryListRecipe & { recipe_name: string })[];
+  items: GroceryListItemWithDetails[];
+}
