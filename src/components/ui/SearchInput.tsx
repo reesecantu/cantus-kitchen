d431891a1346abed9322da09c49f61a6fa705418
reesecantu <65140498+reesecantu@users.noreply.github.com>
2025-09-26 15:18:31 -0400

    Change colors for SearchInput

--- src/components/ui/SearchInput.tsx
+++ src/components/ui/SearchInput.tsx
@@ -15,10 +15,10 @@ export const SearchInput = ({
   className = "",
 }: SearchInputProps) => {
   return (
-    <div className={`flex flex-row my-2 items-center w-full max-w-3xl rounded-full border border-${COLORS.BORDER_MEDIUM} ${className}`}>
+    <div className={`flex flex-row my-2 items-center w-full max-w-3xl rounded-full border-2 ${COLORS.BORDER_PRIMARY} ${className}`}>
       {/* Search Icon */}
       <div className="p-1.5">
-        <Search className="w-5 h-5"/>
+        <Search className={`w-4 h-4 md:w-5 md:h-5 ${COLORS.TEXT_SECONDARY}`}/>
       </div>
       {/* Input Field */}
       <input
@@ -26,7 +26,7 @@ export const SearchInput = ({
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
-        className="w-full pr-10 py-1.5 outline-none"
+        className={`w-full pr-10 md:py-1.5 py-1 outline-none text-${COLORS.TEXT_PRIMARY}`}
       />
     </div>
   );
